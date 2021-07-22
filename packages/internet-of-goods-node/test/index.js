ffc6c9519090d133a0f98964e5c61c4794531f8c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-07-22 22:56:51 +0300

    internet-of-goods-node > test: index

--- packages/internet-of-goods-node/test/index.js
+++ packages/internet-of-goods-node/test/index.js
@@ -5,10 +5,26 @@ const {
 
 
 const main = () => {
-    const simulator = new Simulator([]);
+    const locations = [
+        'A-1-1',
+        'A-1-2',
+        'A-1-3',
+
+        'A-2-1',
+        'A-2-2',
+        'A-2-3',
+
+        'B-1-1',
+        'B-1-2',
+        'B-1-3',
+        'B-1-4',
+        'B-1-5',
+    ];
+
+    const simulator = new Simulator(locations);
     simulator.run();
 
-    simulator.send('A', 'B');
+    simulator.send('A-1-1', 'B-1-5');
 }
 
 main();
