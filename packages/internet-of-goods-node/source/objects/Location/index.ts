2bad34f1b1f3bea63b3e0108dcbb1f3969c06a58
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-07-22 22:52:19 +0300

    internet-of-goods-node > dev: objects/Location

---
+++ packages/internet-of-goods-node/source/objects/Location/index.ts
@@ -0,0 +1,17 @@
+// #region module
+class Location {
+    private value: string;
+
+    constructor(
+        value: string,
+    ) {
+        this.value = value;
+    }
+}
+// #endregion module
+
+
+
+// #region exports
+export default Location;
+// #endregion exports
