f6769caa1cdeccbb5384a2e04adbd9a663b119c4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-07-24 18:25:55 +0300

    internet-of-goods-node > dev: objects/Simulator

--- packages/internet-of-goods-node/source/objects/Simulator/index.ts
+++ packages/internet-of-goods-node/source/objects/Simulator/index.ts
@@ -1,20 +1,65 @@
 // #region imports
+    // #region libraries
+    import {
+        EventEmitter,
+    } from 'events';
+    // #endregion libraries
+
+
     // #region external
     import {
         NodeDefinition,
     } from '~data/interfaces';
 
     import Node from '../Node';
+    import Pod from '../Pod';
     // #endregion external
 // #endregion imports
 
 
 
 // #region module
+class Network extends EventEmitter {
+    private self = new EventEmitter();
+
+
+    constructor() {
+        super();
+
+        this.self.on('addNode', this.addNode);
+        this.self.on('sendPod', this.sendPod);
+    }
+
+
+    private addNode(
+        node: any,
+    ) {
+
+    }
+
+    private sendPod(
+        node: any,
+    ) {
+
+    }
+}
+
+
 class Simulator {
+    private network = new Network();
+
+
     constructor(
         nodes: NodeDefinition[],
     ) {
+        for (const node of nodes) {
+            this.network.emit('addNode', node);
+        }
+    }
+
+
+    private initializeNodes() {
+
     }
 
 
@@ -22,12 +67,8 @@ class Simulator {
         from: string,
         to: string,
     ) {
-
-    }
-
-
-    private initializeNodes() {
-
+        const pod = new Pod(from, to);
+        this.network.emit('sendPod', pod);
     }
 }
 // #endregion module
